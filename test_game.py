34db01b85841afbf56e9a59c3feb28aaa0e61f6f
c0b23086 <c0b230860d@edu.teu.ac.jp>
2024-06-20 20:54:06 +0900

    main関数を変更

--- test_game.py
+++ test_game.py
@@ -73,15 +73,12 @@ def attack_kk_sound()->None:
     Megalovaniaを流す
     """
     pygame.mixer.init()  # 初期化
-    
     # sound = pygame.mixer.Sound("./sound/Chipi_Chipi_Chapa_Chapa.mp3")
     # sound.play()
-
     sound = pygame.mixer.Sound("./sound/Megalovania.mp3")
     sound.play()
     
     # time.sleep(5)
-    
     # とっくさん遊びゾーン
     # now_volume = sound.get_volume()
     # for i in range(40):
@@ -92,41 +89,17 @@ def attack_kk_sound()->None:
     # sound.fadeout(5000)
     # pygame.mixer.music.stop()
     # sound.stop()
-
     return 
 
-def attack_kk(screen, kk_img, kk_rect, hurt_img, hurt_rect)->None:
-    """
-    引数:screen, kk_img, kk_rect, hurt, hurt_rect
-    戻り値:なし
-    こうかとんとの戦闘画面を表示する
-    """
-    
-    screen.blit(kk_img, kk_rect)
 
-    # ハートの動ける範囲
-    pg.draw.rect(screen,(255,255,255), Rect(WIDTH/3, HEIGHT/3+100, WIDTH/3, HEIGHT/2.5), 10)
-
-    #キーボード操作
-    sum_mv = [0, 0]
-    key_lst = pg.key.get_pressed()  # キーが押されているか？
-    for k, v in DELTA.items():
-        if key_lst[k]:
-            sum_mv[0] += v[0]
-            sum_mv[1] += v[1]
-    hurt_rect.move_ip(sum_mv)
-    if check_bound(hurt_rect) != (True, True):
-        hurt_rect.move_ip(-sum_mv[0], -sum_mv[1])
-    screen.blit(hurt_img, hurt_rect)
-
-
-def main():
-    # ゲームの初期化
-    pg.display.set_caption("Kouka tale")  # 画面タイトル設定
-    screen = pg.display.set_mode((WIDTH, HEIGHT))  # 画面用のSurfaceインスタンスを生成
-    
+def test_atk_kk(screen):
     # ハートの読み込み
     # 'rotezoom':読み込んだ画像に回転、拡大縮小を書ける
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound("./sound/Megalovania.mp3")
+    sound.play()
+
+
     hurt_img = pg.transform.rotozoom(
         pg.image.load("fig/Undertale_hurt.png"), 
         0, 0.02
@@ -144,25 +117,51 @@ def main():
     
     clock = pg.time.Clock()  # time
     tmr = 0  # タイマーの初期値
-    
-    # start画面の表示
-    start_game(screen)
-
-    attack_kk_sound()  # Megalovaniaを流す
-    
-    # ゲームのループ
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                return       
-        screen.fill((0, 0, 0))  # 画面の色を指定（背景画像があるときは不要）
+                return   
+        screen.fill((0, 0, 0))  # 画面の色を指定（背景画像があるときは不要）   
         
+        screen.blit(kk_img, kk_rect)
 
-        attack_kk(screen,kk_img,kk_rect,hurt_img,hurt_rect)
+        # ハートの動ける範囲
+        pg.draw.rect(screen,(255,255,255), Rect(WIDTH/3, HEIGHT/3+100, WIDTH/3, HEIGHT/2.5), 10)
 
+        #キーボード操作
+        sum_mv = [0, 0]
+        key_lst = pg.key.get_pressed()  # キーが押されているか？
+        for k, v in DELTA.items():
+            if key_lst[k]:
+                sum_mv[0] += v[0]
+                sum_mv[1] += v[1]
+        hurt_rect.move_ip(sum_mv)
+        if check_bound(hurt_rect) != (True, True):
+            hurt_rect.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(hurt_img, hurt_rect)
         pg.display.update()  # ディスプレイを更新する
         tmr += 1  # 1フレーム事にタイマーに1加える
-        clock.tick(200)  # 1秒あたり何フレーム進むか決める    
+        clock.tick(200)  # 1秒あたり何フレーム進むか決める  
+               
+
+def main():
+    # ゲームの初期化
+    pg.display.set_caption("Kouka tale")  # 画面タイトル設定
+    screen = pg.display.set_mode((WIDTH, HEIGHT))  # 画面用のSurfaceインスタンスを生成
+    
+    # start画面の表示
+    start_game(screen)
+    
+    test_atk_kk(screen)
+    # ゲームのループ
+    # while True:
+    #     for event in pg.event.get():
+    #         if event.type == pg.QUIT:
+    #             return       
+
+        # pg.display.update()  # ディスプレイを更新する
+        # tmr += 1  # 1フレーム事にタイマーに1加える
+        # clock.tick(200)  # 1秒あたり何フレーム進むか決める    
 
 
 if __name__ == "__main__":
